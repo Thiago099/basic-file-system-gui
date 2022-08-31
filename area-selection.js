c812aad71be1384465429618abb91d6e9eb0299c
Thiago099 <tecnologiasthiago@gmail.com>
2022-08-31 12:06:23 -0300

    first commit

---
+++ area-selection.js
@@ -0,0 +1,97 @@
+import './area-selection.css'
+
+export function areaSelectionTool(callback)
+{
+    var rectangle = document.createElement('div')
+    rectangle.classList.add('rectangle')
+    rectangle.style.display = 'none'
+    document.body.appendChild(rectangle)
+    
+    var x = 0
+    var y = 0
+    var left = 0
+    var top = 0
+    var width = 0
+    var height = 0  
+    var drag = false
+    function update_rectangle()
+    {
+        rectangle.style.left = left + 'px'
+        rectangle.style.top = top + 'px'
+        rectangle.style.width = width + 'px'
+        rectangle.style.height = height + 'px'
+    }
+    function mousemove(ev)
+    {
+        const {clientX, clientY} = ev
+        if(clientX < x)
+        {
+            left = clientX
+            width = x - clientX
+        }
+        else
+        {
+            width = clientX - x
+        }
+        if(clientY < y)
+        {
+            top = clientY
+            height = y - clientY
+        }
+        else
+        {
+            height = clientY - y
+        }
+        if(width > 10 || height > 10)
+        {
+            rectangle.style.display = 'block'
+        }
+        update_rectangle()
+        
+    }
+    document.onmousedown = (ev)=>{
+        ev.preventDefault()
+        const {clientX, clientY} = ev
+        x = clientX
+        y = clientY
+        left = clientX
+        top = clientY
+        width = 0
+        height = 0
+        document.onmousemove = mousemove
+        drag = true
+        update_rectangle()
+    
+    }
+    document.onmouseup = (ev)=>{
+        const {clientX, clientY} = ev
+        let left, top, right, bottom
+        if(clientX > x)
+        {
+            left = x
+            right = clientX
+        }
+        else
+        {
+            left = clientX
+            right = x
+        }
+        if(clientY > y)
+        {
+            top = y
+            bottom = clientY
+        }
+        else
+        {
+            top = clientY
+            bottom = y
+        }
+        
+        
+        document.onmousemove = null
+        drag = false
+        update_rectangle()
+        rectangle.style.display = 'none'
+        callback(left, top, right, bottom)
+    }
+}
---
+++ main.js
@@ -0,0 +1,85 @@
+import './style.css'
+
+import '@fortawesome/fontawesome-free/css/all.css'
+
+
+const types = {
+  'folder': ['fa-solid','fa-folder','folder-icon','icon'],
+  'file': ['fa-solid','fa-file','file-icon','icon'],
+}
+function renderElement(parent,name,type)
+{
+  const element = document.createElement('div')
+  element.draggable = true
+  element.classList.add('element')
+  const elementIcon = document.createElement('i')
+  elementIcon.classList.add(...types[type])
+  element.appendChild(elementIcon)
+  const elementName = document.createElement('div')
+  elementName.classList.add('element-name')
+  elementName.innerText = name
+  element.appendChild(elementName)
+  parent.appendChild(element)
+  return element
+}
+import {areaSelectionTool} from './area-selection.js'
+
+
+const app = document.querySelector('#app')
+const elements = []
+elements.push(renderElement(app,'Folder 1', 'folder'))
+elements.push(renderElement(app,'Folder 2', 'folder'))
+elements.push(renderElement(app,'Long folder name', 'folder'))
+elements.push(renderElement(app,'File 3', 'file'))
+
+var ctrlPressed = false
+document.addEventListener('keydown',ev=>{
+  if(ev.key === 'Control')
+  {
+    ctrlPressed = true
+  }
+})
+document.addEventListener('keyup',ev=>{
+  if(ev.key === 'Control')
+  {
+    ctrlPressed = false
+  }
+})
+areaSelectionTool((left,top,right,bottom)=>{
+  for(const element of elements)
+  {
+    // get element left top right and bottom
+    const {offsetLeft,offsetTop,offsetWidth,offsetHeight} = element
+    if(offsetLeft > right || offsetLeft + offsetWidth < left || offsetTop > bottom || offsetTop + offsetHeight < top)
+    {
+      if(!ctrlPressed)
+      {
+        element.classList.remove('element-selected')
+      }
+    }
+    else
+    {
+      if(ctrlPressed)
+      {
+        if(element.classList.contains('element-selected'))
+        {
+          element.classList.remove('element-selected')
+        }
+        else
+        {
+          element.classList.add('element-selected')
+        }
+      }
+      else
+      {
+        element.classList.add('element-selected')
+      }
+    }
+  }
+})
+// for(const element of elements)
+// {
+//   element.addEventListener('dragstart',(ev)=>{
+//     ev.dataTransfer.setData('text/plain',ev.target.innerText)
+//   })
+// }
